1623f5dea374028dc085e26f26f93fc9ce2aeb63
Tim Göttlicher <timg00@outlook.de>
2022-01-28 20:10:51 +0100

    Weight and bias gradients on same plot

--- evaluation/gradientplots.py
+++ evaluation/gradientplots.py
@@ -12,31 +12,47 @@ def load_gradients(path: Path):
     for i, filename in enumerate(sorted(path.glob("gradient_*"))):
         gradients = load_float_txt(filename)
         segments = np.array_split(gradients, 20)
-        name = filename.stem[len("gradient_"):]
-        mean_gradients[name] = [np.mean(arr) for arr in segments]
+
+        param_name = filename.stem[len("gradient_"):]
+        layer_name, type = param_name.rsplit('.', maxsplit = 1)
+
+        if layer_name not in mean_gradients:
+            mean_gradients[layer_name] = dict()
+
+        mean_gradients[layer_name][type] = [np.mean(arr) for arr in segments]
 
     return mean_gradients
 
 
-def generate_gradient_grid(gradients_data):
+def generate_gradient_grid(layer_data):
     plt.rc('axes', titlesize=6)
 
     columns = 8
-    rows = len(gradients_data) // columns + 1
+    rows = len(layer_data) // columns + 1
     print(columns, " x ", rows)
 
     fig, axs = plt.subplots(rows, columns, sharey=True, sharex=True, squeeze=False, figsize=(25,25))
 
-    for i, (name, gradients) in enumerate(gradients_data.items()):
+    for i, (name, gradients_data) in enumerate(layer_data.items()):
         row = i // columns
         col = i % columns
-        ind = range(len(gradients))
+        ax = axs[row, col]
 
-        axs[row, col].fill_between(ind, gradients)
-        axs[row, col].set_title(name, wrap=True, pad=0.8)
-        axs[row, col].set_xticks([])
+        for param, gradients in gradients_data.items():
+            ind = range(len(gradients))
+            ax.fill_between(ind, gradients, label=param, alpha=0.5)
 
-    for i in range(len(gradients_data), rows * columns):
+        for param, gradients in gradients_data.items():
+            ind = range(len(gradients))
+            ax.plot(ind, gradients, label=param)
+
+        ax.set_title(name, wrap=True, pad=0.8)
+        ax.set_xticks([])
+
+        if i == 0:
+            ax.legend()
+
+    for i in range(len(layer_data), rows * columns):
         row = i // columns
         col = i % columns
         axs[row, col].axis('off')
