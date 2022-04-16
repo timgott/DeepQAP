ad87eaf3cb6e9cd10205a8594bdbc917d49c724e
Tim Göttlicher <timg00@outlook.de>
2022-04-16 14:04:47 +0200

    Fix script

--- run_smallrandom.sh
+++ run_smallrandom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in $(seq 8); do
-    python src/drlqap/train_agent.py runs/$1_smallrandoms/s$i $1 small_random_graphs $i &;
+    python src/drlqap/train_agent.py runs/$1_smallrandoms/s$i $1 small_random_graphs $i &
 done
