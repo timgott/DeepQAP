3e65fe420ba9ac24de35f973d7b8d3cdd7d6f506
Tim Göttlicher <timg00@outlook.de>
2022-04-20 01:43:09 +0200

    Fix typo in dqn script

--- run_dqn_smallrandom.sh
+++ run_dqn_smallrandom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-agent=dqn_dense_mc_ec_eps0
+agent=dqn_dense_ms_ec_eps0
 for i in $(seq 4); do
     for lr in 1e-5 5e-4 3e-4 1e-4; do
         python scripts/train_agent.py runs/"$agent"_smallrandoms/s$i $agent small_random_graphs --seed $i -lr $lr;
