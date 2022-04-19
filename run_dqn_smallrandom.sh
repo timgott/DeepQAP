03190bc5a02262a1438a435e59c6b648a683c353
Tim Göttlicher <timg00@outlook.de>
2022-04-20 01:45:23 +0200

    Add param to experiment path name

--- run_dqn_smallrandom.sh
+++ run_dqn_smallrandom.sh
@@ -2,6 +2,6 @@
 agent=dqn_dense_ms_ec_eps0
 for i in $(seq 4); do
     for lr in 1e-5 5e-4 3e-4 1e-4; do
-        python scripts/train_agent.py runs/"$agent"_smallrandoms/s$i $agent small_random_graphs --seed $i -lr $lr;
+        python scripts/train_agent.py runs/"$agent"_smallrandoms/lr"$lr"_s"$i" $agent small_random_graphs --seed $i -lr $lr;
     done
 done
