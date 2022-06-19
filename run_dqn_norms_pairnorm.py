2906f5730859f3254c03bba44f787d3b59e704fb
Tim Göttlicher <timg00@outlook.de>
2022-06-19 09:41:45 +0000

    Norm evaluation runs

---
+++ run_dqn_norms_pairnorm.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from drlqap.experiment import run_experiment_from_config
+
+agent = "dqn_dense_ec_eps0"
+for seed in range(4):
+    for depth in [2]:
+        for norm in ['pair_norm']:
+            args = {
+                'learning_rate': 5e-4,
+                'gnn_norm': norm,
+                'gnn_depth': depth
+            }
+            path = f'runs/{agent}_norm_study/s{seed}_{norm}'
+            run_experiment_from_config(
+                experiment_path=path,
+                agent_name=agent,
+                task_name="small_random_graphs",
+                agent_arguments=args,
+                seed=seed
+            )
